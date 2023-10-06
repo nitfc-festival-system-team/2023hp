c64476d6f1710eb186cdb296e05c37c5033ed180
void2610 <izumisan2610@gmail.com>
2023-10-07 02:48:14 +0900

    feat: 全体のstyleを変更できるようにした

--- src/app/ChakraProvider.tsx
+++ src/app/ChakraProvider.tsx
@@ -1,24 +1,9 @@
 "use client";
-
 import React from "react";
 import { ChakraProvider as Base } from "@chakra-ui/react";
 import { CacheProvider } from "@chakra-ui/next-js";
 
-import "@/styles/globals.css";
-
-// 1. Import the extendTheme function
-import { extendTheme } from "@chakra-ui/react";
-
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-export const theme = extendTheme({ colors });
+import { globalTheme } from "@/styles/globalsTheme";
 
 export default function ChakraProvider({
   children,
@@ -27,7 +12,7 @@ export default function ChakraProvider({
 }) {
   return (
     <CacheProvider>
-      <Base theme={theme}>{children}</Base>
+      <Base theme={globalTheme}>{children}</Base>
     </CacheProvider>
   );
 }
