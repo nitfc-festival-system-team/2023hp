c352c166d20cac6f5f70ad5dc51e2cdefc8772bc
Fill71 <52400184+Fill71@users.noreply.github.com>
2023-10-08 00:53:01 +0900

    feat: 0.1sごとに文字を1文字ずつ表示

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,17 +1,30 @@
+import { useState, useEffect } from "react";
 import "../styles/title.css";
 
 export const Title = () => {
+  const titletxt = "福井高専高専祭".split("");
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div
-      style={{
-        fontFamily: "Zen Maru Gothic, sans-serif",
-        marginTop: "50px",
-        letterSpacing: 10,
-        fontSize: 70,
-        writingMode: "vertical-rl",
-      }}
-    >
-      福井高専祭
+    <div className="title">
+      {titletxt.map((char, index) => (
+        <span
+          key={index}
+          className={index <= count ? "fadeIn" : ""}
+          style={{ animationDelay: `${index * 0.1}s`, opacity: 0 }}
+        >
+          {char}
+        </span>
+      ))}
     </div>
   );
 };
