33a83bfabe7c10030565de38f8c3f59a2b16c02a
Fill71 <52400184+Fill71@users.noreply.github.com>
2023-10-07 20:06:19 +0900

    style: titleのフォント、文字の大きさ、文字間隔の変更

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,3 +1,17 @@
+import "../styles/title.css";
+
 export const Title = () => {
-  return <div style={{ writingMode: "vertical-rl" }}>福井高専祭</div>;
+  return (
+    <div
+      style={{
+        fontFamily: "Zen Maru Gothic, sans-serif",
+        marginTop: "50px",
+        letterSpacing: 10,
+        fontSize: 70,
+        writingMode: "vertical-rl",
+      }}
+    >
+      福井高専祭
+    </div>
+  );
 };
