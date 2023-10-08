3711aa054f66996510bd19ec771032b53d161dfe
Fill71 <52400184+Fill71@users.noreply.github.com>
2023-10-09 02:03:10 +0900

    Fix : 修正点を修正

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 import styles from "../styles/title.module.css";
 
 export const Title = () => {
-  const titletxt = "福井高専 高専祭".split("");
+  const titleText = "福井高専 高専祭".split("");
   // 文字が浮き上がってくる時間の調整;
   const intervaltime = 250;
 
   const [count, setCount] = useState(0);
-
+  // CSS のプロパティ テキストの行のレイアウトを横書きにするか縦書きにするか、ブロックのフロー方向を設定
+  //vertical-rlでは上から下へ垂直に流れ、次の垂直の行は、前の行の左に配置
   type WritingMode = "horizontal-tb" | "vertical-rl" | "vertical-lr";
 
   const writingModeValue: WritingMode | undefined =
@@ -38,7 +39,7 @@ export const Title = () => {
   return (
     <div>
       <p style={titleStyle}>
-        {titletxt.map((char, index) => (
+        {titleText.map((char, index) => (
           <span
             key={index}
             className={index <= count ? styles.fadeIn : ""}
@@ -52,7 +53,6 @@ export const Title = () => {
           </span>
         ))}
       </p>
-      {/* <p>10.27/10.30</p> */}
     </div>
   );
 };
