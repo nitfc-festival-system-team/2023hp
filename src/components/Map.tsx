2cb447693dee6222d1b7b9edc626e743f74a1bd8
suisosuii <hayatokomiyama@icloud.com>
2023-10-09 04:33:20 +0900

    押してみな、とぶぞ
    どこかを押すとログ出るよ

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,34 +1,50 @@
 import Link from "next/link";
-
 import { useRouter } from "next/router";
-
 import { MouseEvent } from "react";
+import React, { useState } from "react";
 
 export const Map = () => {
   const router = useRouter();
 
-  const handleAreaClick = (e: MouseEvent) => {
-    const target = e.target as HTMLAreaElement; // ターゲット要素をHTMLAreaElement型にキャスト
+  const [clickedArea, setClickedArea] = useState<string | null>(null);
+  const [clickedCoords, setClickedCoords] = useState<string | null>(null);
 
-    if (target.id === "area1") {
-      router.push("../pages/index.tsx");
-    } else if (target.id === "area2") {
-      router.push("../pages/index.tsx");
+  const handleAreaClick = (e: React.MouseEvent<HTMLAreaElement>) => {
+    const areaId = e.currentTarget.id;
+    const areaCoords = e.currentTarget.getAttribute("coords");
+    setClickedArea(areaId);
+    setClickedCoords(areaCoords);
+    if (areaId === "area1") {
+      console.log("area1");
+    } else if (areaId === "area2") {
+      console.log("お前はarea2を押したんだぞ");
     }
   };
+
   return (
     <div>
-      <img
-        src="./image/campus_map.png"
-        // alt="Map"
-        className="unko"
-        // useMap="#map"
-        // onClick={handleAreaClick}
-      />
+      <img src="/image/campus_map.gif" alt="Map" useMap="#map" />
       <map name="map">
-        <area id="area1" shape="rect" coords="100,100,200,200" />
-        <area id="area2" shape="rect" coords="800,100,900,200" />
+        <area
+          id="area1"
+          shape="rect"
+          coords="10,20,100,80"
+          onClick={handleAreaClick}
+        />
+        <area
+          id="area2"
+          shape="rect"
+          coords="110,20,200,80"
+          onClick={handleAreaClick}
+        />
       </map>
+      {clickedArea && (
+        <div>
+          選択したエリア: {clickedArea}
+          <br />
+          クリックされた座標: {clickedCoords}
+        </div>
+      )}
     </div>
   );
 };
