450dc37220b133f7a8ff2ef1fcfe7dadc2bf989a
Fill71 <52400184+Fill71@users.noreply.github.com>
2023-10-08 18:14:55 +0900

    refac: 表示時間の調整

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,12 +4,14 @@ import "../styles/title.css";
 export const Title = () => {
   const titletxt = "福井高専高専祭".split("");
 
+  const intervaltime = 250;
+
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCount((count) => count + 1);
-    }, 500);
+    }, intervaltime);
 
     return () => clearInterval(interval);
   }, []);
@@ -20,7 +22,10 @@ export const Title = () => {
         <span
           key={index}
           className={index <= count ? "fadeIn" : ""}
-          style={{ animationDelay: `${index * 0.1}s`, opacity: 0 }}
+          style={{
+            animationDelay: `${index * (intervaltime / 1000)}s`,
+            opacity: 0,
+          }}
         >
           {char}
         </span>
