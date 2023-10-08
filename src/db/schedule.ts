4584a13e38b2893b3a2daf0841d9879f1ce936b7
void2610 <izumisan2610@gmail.com>
2023-10-09 03:08:49 +0900

    feat: schedule作成

---
+++ src/db/schedule.ts
@@ -0,0 +1,19 @@
+import { ScheduleType } from "@/types/schedule";
+
+export const notice: ScheduleType[] = [
+  {
+    title: "イベント1",
+    startDate: new Date(2023, 10, 27, 9, 0),
+    endDate: new Date(2023, 10, 27, 10, 0),
+  },
+  {
+    title: "イベント2",
+    startDate: new Date(2023, 10, 27, 10, 0),
+    endDate: new Date(2023, 10, 27, 11, 0),
+  },
+  {
+    title: "イベント3",
+    startDate: new Date(2023, 10, 27, 11, 0),
+    endDate: new Date(2023, 10, 27, 12, 0),
+  },
+];
---
+++ src/types/schedule.ts
@@ -0,0 +1,5 @@
+export type ScheduleType = {
+  title: string;
+  startDate: Date;
+  endDate?: Date;
+};
