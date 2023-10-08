88f04744838b0aea54c07f6d1cb2717b8424fd3e
Fill71 <52400184+Fill71@users.noreply.github.com>
2023-10-08 18:36:06 +0900

    refac:タグの整理
    コメントアウトしてる部分はいらなかったら消してください

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import "../styles/title.css";
 
 export const Title = () => {
   const titletxt = "福井高専高専祭".split("");
-
+  // 文字が浮き上がってくる時間の調整;
   const intervaltime = 250;
 
   const [count, setCount] = useState(0);
@@ -17,19 +17,22 @@ export const Title = () => {
   }, []);
 
   return (
-    <div className="title">
-      {titletxt.map((char, index) => (
-        <span
-          key={index}
-          className={index <= count ? "fadeIn" : ""}
-          style={{
-            animationDelay: `${index * (intervaltime / 1000)}s`,
-            opacity: 0,
-          }}
-        >
-          {char}
-        </span>
-      ))}
+    <div>
+      <p className="title">
+        {titletxt.map((char, index) => (
+          <span
+            key={index}
+            className={index <= count ? "fadeIn" : ""}
+            style={{
+              animationDelay: `${index * (intervaltime / 1000)}s`,
+              opacity: 0,
+            }}
+          >
+            {char}
+          </span>
+        ))}
+      </p>
+      {/* <p>10.27/10.30</p> */}
     </div>
   );
 };
