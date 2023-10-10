2fdbc7023d13d88afeb781a53011a578f168a510
suisosuii <hayatokomiyama@icloud.com>
2023-10-11 02:29:04 +0900

    リンクマークの色を色相環の回転で変更

--- src/components/StandList.tsx
+++ src/components/StandList.tsx
@@ -23,28 +23,34 @@ const StandItem = ({ stand }: { stand: StandType }) => {
   return (
     <div>
       <h2>
-        {stand.url && (
-          <div onClick={handleRedirect} style={{ cursor: "pointer" }}>
-            <span
-              style={{
-                textAlignLast: "justify",
-                display: "inline-block",
-                width: "70px",
-              }}
-            >
-              {stand.organizer}
-            </span>
+        <span
+          style={{
+            textAlignLast: "justify",
+            display: "inline-block",
+            width: "70px",
+          }}
+        >
+          {stand.organizer}
+        </span>
 
-            <span>
-              {stand.name + ": " + stand.place}
-              <img
-                src="/image/gadget24.png"
-                alt="link mark"
-                width="3%"
-                height="3%"
-              />
-            </span>
-          </div>
+        {stand.name + ": " + stand.place}
+        {stand.url && (
+          <span
+            onClick={handleRedirect}
+            style={{
+              cursor: "pointer",
+              filter:
+                "invert(8%) sepia(100%) saturate(7044%) hue-rotate(227deg) brightness(100%) contrast(125%)",
+              marginLeft: "2em",
+            }}
+          >
+            <img
+              src="/image/gadget24.png"
+              alt="link mark"
+              width="3%"
+              height="3%"
+            ></img>
+          </span>
         )}
       </h2>
     </div>
