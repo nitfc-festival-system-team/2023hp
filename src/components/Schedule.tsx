894e9d8c93a9827ee8307956cfc200f0cbbaaddb
suisosuii <hayatokomiyama@icloud.com>
2023-10-11 16:41:10 +0900

    33-スケジュール表示を作る
    見れる用

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,3 +1,50 @@
+import { schedules } from "@/db/schedule";
+import { title } from "process";
+
+import dayjs from "dayjs";
+import RcGantt, { enUS } from "rc-gantt";
+import React, { useState } from "react";
+
+const now = new Date();
+
+const sorted = schedules.sort((a, b) => {
+  return a.startDate.getTime() - b.startDate.getTime();
+});
+
+console.log(sorted);
+
+interface Data {
+  title: string;
+  startDate: string;
+  endDate?: string;
+}
+
+const data: Data[] = sorted.map((item) => {
+  return {
+    title: item.title,
+    startDate: item.startDate.toDateString(),
+    endDate: item.endDate?.toDateString(),
+  };
+});
+
 export const Schedule = () => {
-  return <div>スケジュール</div>;
+  console.log(data);
+  return (
+    <div style={{ width: "100%", height: 500 }}>
+      <RcGantt<Data>
+        data={data}
+        locale={enUS}
+        columns={[
+          {
+            name: "title",
+            label: "Custom Title",
+            width: 100,
+          },
+        ]}
+        onUpdate={async () => {
+          return true;
+        }}
+      />
+    </div>
+  );
 };
--- src/db/schedule.ts
+++ src/db/schedule.ts
@@ -4,7 +4,7 @@ export const schedules: ScheduleType[] = [
   {
     title: "イベント1",
     place: "メインステージ",
-    startDate: new Date(2023, 10, 27, 9, 0),
+    startDate: new Date(2023, 10, 27, 15, 0),
   },
   {
     title: "イベント2",
