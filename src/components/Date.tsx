5c38605b6dd613d6ff907b400766faecdd6eda2b
Fill71 <52400184+Fill71@users.noreply.github.com>
2023-10-14 00:57:53 +0900

    feat: 日時の後ろに背景を追加

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,3 +1,42 @@
+import styled from "styled-components";
+
+const DateStyle = styled.div`
+  color: black;
+  width: 5vw;
+  height: 5vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+`;
+
+const Tryangle = styled.div`
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-top: 1vw solid transparent;
+  border-bottom: 1vw solid transparent;
+  border-left: 1vw solid #555555;
+  border-right: 0;
+  relative: 5vw;
+`;
+const BackgroundStyle = styled.div`
+  background: rgba(172, 230, 224, 0.7);
+  width: 30vw;
+  display: flex;
+  justify-content: center;
+`;
+
 export const Date = () => {
-  return <div>10/2710/29</div>;
+  return (
+    <BackgroundStyle>
+      <div>
+        <DateStyle>
+          10.27&nbsp;
+          <Tryangle></Tryangle>
+          &nbsp;10.29
+        </DateStyle>
+      </div>
+    </BackgroundStyle>
+  );
 };
