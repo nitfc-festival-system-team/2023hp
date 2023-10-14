8bd4498366c176ac2cf904854d23a2c341efd5c4
Fill71 <52400184+Fill71@users.noreply.github.com>
2023-10-15 00:34:34 +0900

    Fix : 矢印の修正

--- src/components/OpenDate.tsx
+++ src/components/OpenDate.tsx
@@ -8,24 +8,41 @@ const DateStyle = styled.div`
   align-items: center;
   justify-content: center;
   font-weight: bold;
-  font-size: 2vw;
+  font-size: 3vw;
   padding: 10px;
   text-align: center;
 `;
 
 const BackgroundStyle = styled.div`
   background: rgba(172, 230, 224, 0.7);
-  width: 20vw;
+  width: 30vw;
   height: auto;
   display: flex;
   justify-content: center;
+`;
+
+const RightArrowAfter = styled.div`
+  position: relative;
+  width: 1vw;
+  height: 1vw;
+  display: flex;
+  border-top: 0.5vw solid black;
+  border-right: 0.5vw solid black;
+  -webkit-transform: rotate(45deg);
+  transform: rotate(45deg);
 `;
 
 export const OpenDate = () => {
   return (
     <BackgroundStyle>
       <div>
-        <DateStyle>10.27&nbsp;▶&nbsp;10.29</DateStyle>
+        <DateStyle>
+          10.27&nbsp;
+          <span>
+            <RightArrowAfter />
+          </span>
+          &nbsp;10.29
+        </DateStyle>
       </div>
     </BackgroundStyle>
   );
