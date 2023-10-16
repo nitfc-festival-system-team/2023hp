088f2ae58914fe470b60b79a4c719e374e7d42ad
void2610 <izumisan2610@gmail.com>
2023-10-16 23:57:44 +0900

    feat: api routerのテスト実装

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,6 @@
+// pages/api/example.ts
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ name: "John Doe" });
+}
