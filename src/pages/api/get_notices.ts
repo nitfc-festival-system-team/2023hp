2b8371cd558a465d7ae949bea2459832862a93fc
void2610 <izumisan2610@gmail.com>
2023-10-17 00:33:25 +0900

    fix: API定義の修正

--- src/pages/api/get_notices.ts
+++ src/pages/api/get_notices.ts
@@ -1,7 +1,10 @@
-import { NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 
-export default async function getData(res: NextApiResponse) {
+export default async function getData(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   const prisma = new PrismaClient();
 
   try {
