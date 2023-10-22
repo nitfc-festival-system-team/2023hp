0ef393a7a532ac6bb93c197abbe9951f4af4566d
void2610 <izumisan2610@gmail.com>
2023-10-23 00:14:01 +0900

    chore: fmt

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,6 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var client_1 = require("@prisma/client");
 var prisma = global.prisma || new client_1.PrismaClient();
-if (process.env.NODE_ENV === "development")
-    global.prisma = prisma;
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 exports.default = prisma;
