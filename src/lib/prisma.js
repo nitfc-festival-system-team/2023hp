762e87bf786dd641122f79c14ff2e2be6a2a264f
void2610 <izumisan2610@gmail.com>
2023-10-22 11:28:28 +0900

    feat: DB削除用スクリプト追加

---
+++ src/lib/prisma.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var client_1 = require("@prisma/client");
+var prisma = global.prisma || new client_1.PrismaClient();
+if (process.env.NODE_ENV === "development")
+    global.prisma = prisma;
+exports.default = prisma;
