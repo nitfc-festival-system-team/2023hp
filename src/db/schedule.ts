a21b360688a4331d1c1a85fe794959f6d925bc40
s55531sf <free34game@icloud.com>
2023-10-23 22:48:59 +0900

    全角ビックリマークから半角に変換
    全角ビックリマークから半角に変換しました。

--- src/db/schedule.ts
+++ src/db/schedule.ts
@@ -4,7 +4,7 @@ export const schedules: ScheduleType[] = [
   {
     title: "オープニングセレモニー",
     place: "第1体育館",
-    description: "共に輝く！始まる文化の祭典へ！",
+    description: "共に輝く!始まる文化の祭典へ!",
     startDate: new Date(2023, 10, 27, 12, 30),
     endDate: new Date(2023, 10, 27, 13, 0),
   },
@@ -12,7 +12,7 @@ export const schedules: ScheduleType[] = [
     title: "気配切り",
     place: "第1体育館",
     description:
-      "感覚を研ぎ澄ませ！自分を信じて、いざ一振り！！決着の様子は体育館で",
+      "感覚を研ぎ澄ませ!自分を信じて、いざ一振り!!決着の様子は体育館で",
     startDate: new Date(2023, 10, 27, 13, 20),
     endDate: new Date(2023, 10, 27, 14, 30),
   },
@@ -28,7 +28,7 @@ export const schedules: ScheduleType[] = [
     title: "高専の乱",
     place: "グラウンド",
     description:
-      "水鉄砲で狙い撃ち！学科同士の天下分け目の闘い！！",
+      "水鉄砲で狙い撃ち!学科同士の天下分け目の闘い!!",
     startDate: new Date(2023, 10, 27, 15, 0),
     endDate: new Date(2023, 10, 27, 16, 0),
   },
@@ -36,7 +36,7 @@ export const schedules: ScheduleType[] = [
     title: "コスプレ",
     place: "第1体育館",
     description:
-      "好きなキャラに変身！どんな奴が出てくるかPVをチェック！！",
+      "好きなキャラに変身!どんな奴が出てくるかPVをチェック!!",
     startDate: new Date(2023, 10, 28, 10, 30),
     endDate: new Date(2023, 10, 28, 12, 40),
   },
@@ -68,7 +68,7 @@ export const schedules: ScheduleType[] = [
     title: "カラオケ大会",
     place: "サブステージ",
     description:
-      "響けわれらの歌声よ！高専祭に華の音色を！！",
+      "響けわれらの歌声よ!高専祭に華の音色を!!",
     startDate: new Date(2023, 10, 28, 13, 40),
     endDate: new Date(2023, 10, 28, 15, 40),
   },
@@ -76,7 +76,7 @@ export const schedules: ScheduleType[] = [
     title: "ラブライブ",
     place: "メインステージ",
     description:
-      "スクールアイドルの軌跡！虹色のステージへ！",
+      "スクールアイドルの軌跡!虹色のステージへ!",
     startDate: new Date(2023, 10, 28, 18, 0),
     endDate: new Date(2023, 10, 28, 19, 10),
   },
@@ -84,7 +84,7 @@ export const schedules: ScheduleType[] = [
     title: "スマブラ大会",
     place: "第1体育館",
     description:
-      "日頃の恨みをゲームの中で！今年もやります♡スマブラ大会",
+      "日頃の恨みをゲームの中で!今年もやります♡スマブラ大会",
     startDate: new Date(2023, 10, 29, 10, 0),
     endDate: new Date(2023, 10, 29, 12, 40),
   },
@@ -100,7 +100,7 @@ export const schedules: ScheduleType[] = [
     title: "ビンゴ大会",
     place: "メインステージ",
     description:
-      "運否天賦！～幸運は天によって定められ与えられるもの～",
+      "運否天賦!～幸運は天によって定められ与えられるもの～",
     startDate: new Date(2023, 10, 29, 15, 40),
     endDate: new Date(2023, 10, 29, 18, 40),
   },
