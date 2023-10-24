aeaea6e365e38d21f753a8f6e5b3fd1607bde752
void2610 <izumisan2610@gmail.com>
2023-10-24 16:33:59 +0900

    style: 縦線のサイズ変更

--- src/components/VerticalLine.tsx
+++ src/components/VerticalLine.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { motion } from "framer-motion";
 
-const ScrollIndicator = () => {
+const PcScrollIndicator = () => {
   const [scrollHeight, setScrollHeight] = useState(0);
 
   useEffect(() => {
@@ -30,7 +30,7 @@ const ScrollIndicator = () => {
         bottom: 0,
         left: 0,
         top: 0,
-        width: "42px",
+        width: "3vw",
       }}
     >
       <motion.div
@@ -45,10 +45,53 @@ const ScrollIndicator = () => {
   );
 };
 
-const VerticalLine: React.FC = () => {
+const MobileScrollIndicator = () => {
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const totalHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const scrollProgress = scrollTop / totalHeight;
+      setScrollHeight(scrollProgress * 100);
+    };
+
+    handleScroll(); // 初期位置を設定
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        top: 0,
+        width: "6vw",
+      }}
+    >
+      <motion.div
+        style={{
+          width: "100%",
+          height: `${scrollHeight}%`,
+          transformOrigin: "0% 100%",
+          backgroundColor: "black",
+        }}
+      />
+    </div>
+  );
+};
+
+const MobileVerticalLine: React.FC = () => {
   const divStyle: React.CSSProperties = {
     position: "fixed",
-    left: "42px",
+    left: "6vw",
     top: 0,
     height: "100%",
     width: "1px",
@@ -58,10 +101,42 @@ const VerticalLine: React.FC = () => {
   const textStyle: React.CSSProperties = {
     position: "fixed",
     left: "0",
-    top: "13%",
+    top: "30vw",
     writingMode: "vertical-rl",
     textAlign: "center",
-    fontSize: "35px",
+    fontSize: "6vw",
+    fontFamily: "KomorebiFont",
+    color: "white",
+    mixBlendMode: "difference", // スクロールバーと重なる部分の文字を白くする
+  };
+
+  return (
+    <>
+      <div style={divStyle}></div>
+      <div style={textStyle}>
+        <span>NITFC FESTIVAL 2023</span>
+      </div>
+    </>
+  );
+};
+
+const PcVerticalLine: React.FC = () => {
+  const divStyle: React.CSSProperties = {
+    position: "fixed",
+    left: "3vw",
+    top: 0,
+    height: "100%",
+    width: "1px",
+    backgroundColor: "#000",
+  };
+
+  const textStyle: React.CSSProperties = {
+    position: "fixed",
+    left: "0",
+    top: "10vw",
+    writingMode: "vertical-rl",
+    textAlign: "center",
+    fontSize: "3vw",
     fontFamily: "KomorebiFont",
     color: "white",
     mixBlendMode: "difference", // スクロールバーと重なる部分の文字を白くする
@@ -78,10 +153,28 @@ const VerticalLine: React.FC = () => {
 };
 
 export const Sidebar = () => {
-  return (
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const userAgent = window.navigator.userAgent;
+
+    const mobile =
+      userAgent.includes("iPhone") ||
+      userAgent.includes("Android") ||
+      userAgent.includes("iPad");
+
+    setIsMobile(mobile);
+  }, []);
+
+  return isMobile ? (
     <>
-      <ScrollIndicator />
-      <VerticalLine />
+      <MobileScrollIndicator />
+      <MobileVerticalLine />
+    </>
+  ) : (
+    <>
+      <PcScrollIndicator />
+      <PcVerticalLine />
     </>
   );
 };
