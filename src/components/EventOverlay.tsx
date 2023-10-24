a71dc18cf13f09eda6c4fd9f25c6e7b5091649cd
void2610 <izumisan2610@gmail.com>
2023-10-24 15:55:29 +0900

    feat: イベント表示が改行されないようにした

--- src/components/EventOverlay.tsx
+++ src/components/EventOverlay.tsx
@@ -45,6 +45,7 @@ const SubTitle = styled.h3`
 
 const Title = styled.h2`
   margin: 0;
+  white-space: nowrap;
 `;
 
 const ToggleButton = styled.button`
@@ -117,8 +118,8 @@ export const EventHeader = () => {
                     {nowHeld.length !== 0 &&
                     nowHeld[0].startDate.getTime() >
                       upcomingEvent.startDate.getTime()
-                      ? "注目!"
-                      : "Next"}
+                      ? "開催中！"
+                      : "Next Event"}
                   </SubTitle>
                   {/* 現在開催中のイベントがないor次のイベントまで10分を切っていたら
                   次のイベント、それ以外なら現在開催中のイベントを表示 */}
