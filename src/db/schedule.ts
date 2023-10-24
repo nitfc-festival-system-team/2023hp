8547b5f651f1cc9261d3bf3b1734eb83656ced2a
void2610 <izumisan2610@gmail.com>
2023-10-24 10:15:33 +0900

    chore: fmt

--- src/db/schedule.ts
+++ src/db/schedule.ts
@@ -19,104 +19,91 @@ export const schedules: ScheduleType[] = [
   {
     title: "FREE STYLE MMC",
     place: "第1体育館",
-    description:
-      "",
+    description: "",
     startDate: new Date(2023, 10, 27, 15, 0),
     endDate: new Date(2023, 10, 27, 17, 0),
   },
   {
     title: "高専の乱",
     place: "グラウンド",
-    description:
-      "水鉄砲で狙い撃ち!学科同士の天下分け目の闘い!!",
+    description: "水鉄砲で狙い撃ち!学科同士の天下分け目の闘い!!",
     startDate: new Date(2023, 10, 27, 15, 0),
     endDate: new Date(2023, 10, 27, 16, 0),
   },
   {
     title: "コスプレ",
     place: "第1体育館",
-    description:
-      "好きなキャラに変身!どんな奴が出てくるかPVをチェック!!",
+    description: "好きなキャラに変身!どんな奴が出てくるかPVをチェック!!",
     startDate: new Date(2023, 10, 28, 10, 30),
     endDate: new Date(2023, 10, 28, 12, 40),
   },
   {
     title: "吹奏楽部",
     place: "メインステージ",
-    description:
-      "",
+    description: "",
     startDate: new Date(2023, 10, 28, 12, 40),
     endDate: new Date(2023, 10, 28, 14, 40),
   },
   {
     title: "少林寺拳法",
     place: "メインステージ",
-    description:
-      "",
+    description: "",
     startDate: new Date(2023, 10, 28, 14, 40),
     endDate: new Date(2023, 10, 28, 16, 0),
   },
   {
     title: "ダンス同好会",
     place: "メインステージ",
-    description:
-      "",
+    description: "",
     startDate: new Date(2023, 10, 28, 16, 0),
     endDate: new Date(2023, 10, 28, 17, 10),
   },
   {
     title: "カラオケ大会",
     place: "サブステージ",
-    description:
-      "響けわれらの歌声よ!高専祭に華の音色を!!",
+    description: "響けわれらの歌声よ!高専祭に華の音色を!!",
     startDate: new Date(2023, 10, 28, 13, 40),
     endDate: new Date(2023, 10, 28, 15, 40),
   },
   {
     title: "ラブライブ",
     place: "メインステージ",
-    description:
-      "スクールアイドルの軌跡!虹色のステージへ!",
+    description: "スクールアイドルの軌跡!虹色のステージへ!",
     startDate: new Date(2023, 10, 28, 18, 0),
     endDate: new Date(2023, 10, 28, 19, 10),
   },
   {
     title: "スマブラ大会",
     place: "第1体育館",
-    description:
-      "日頃の恨みをゲームの中で!今年もやります♡スマブラ大会",
+    description: "日頃の恨みをゲームの中で!今年もやります♡スマブラ大会",
     startDate: new Date(2023, 10, 29, 10, 0),
     endDate: new Date(2023, 10, 29, 12, 40),
   },
   {
     title: "FREE STYLE MMC",
     place: "メインステージ",
-    description:
-      "",
+    description: "",
     startDate: new Date(2023, 10, 29, 12, 0),
     endDate: new Date(2023, 10, 29, 15, 0),
   },
   {
     title: "ビンゴ大会",
     place: "メインステージ",
-    description:
-      "運否天賦!～幸運は天によって定められ与えられるもの～",
+    description: "運否天賦!～幸運は天によって定められ与えられるもの～",
     startDate: new Date(2023, 10, 29, 15, 40),
     endDate: new Date(2023, 10, 29, 18, 40),
   },
   {
     title: "エンディングセレモニー",
     place: "メインステージ",
-    description:
-      "感動の幕引き、文化祭のフィナーレ",
+    description: "感動の幕引き、文化祭のフィナーレ",
     startDate: new Date(2023, 10, 29, 18, 40),
     endDate: new Date(2023, 10, 29, 19, 0),
   },
   {
     title: "花火大会",
     place: "福井",
-    description:
-      "今夜限りの光あるお花畑♡",
+    description: "今夜限りの光あるお花畑♡",
     startDate: new Date(2023, 10, 29, 19, 0),
     endDate: new Date(2023, 10, 29, 19, 15),
   },
