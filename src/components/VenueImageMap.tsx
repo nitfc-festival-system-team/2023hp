1f4a8944cab537e667201726e310864b18a3ff01
void2610 <izumisan2610@gmail.com>
2023-10-26 00:46:40 +0900

    refac: queryパラメータを設定

--- src/components/VenueImageMap.tsx
+++ src/components/VenueImageMap.tsx
@@ -1,6 +1,12 @@
+import React from "react";
+
 import { useCheckIsMobile } from "@/features/checkIsMobile";
 
-export const VenueMap = () => {
+export const VenueMap = ({
+  onAreaClick,
+}: {
+  onAreaClick: (standNumber: number) => void;
+}) => {
   const [isMobile, device] = useCheckIsMobile();
 
   return (
@@ -22,158 +28,184 @@ export const VenueMap = () => {
         <area
           shape="rect"
           coords="270,387,297,412"
-          href="/contents/stand#1"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(1)}
         />
         <area
           shape="rect"
           coords="271,412,297,436"
-          href="/contents/stand#2"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(2)}
         />
         <area
           shape="rect"
           coords="273,457,299,481"
-          href="/contents/stand#3"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(3)}
         />
         <area
           shape="rect"
           coords="272,481,298,504"
-          href="/contents/stand#4"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(4)}
         />
         <area
           shape="rect"
           coords="184,384,209,418"
-          href="/contents/stand#5"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(5)}
         />
         <area
           shape="rect"
           coords="184,418,208,450"
-          href="/contents/stand#6"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(6)}
         />
         <area
           shape="rect"
           coords="183,455,210,487"
-          href="/contents/stand#7"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(7)}
         />
         <area
           shape="rect"
           coords="182,487,210,520"
-          href="/contents/stand#8"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(8)}
         />
         <area
           shape="rect"
           coords="213,590,251,611"
-          href="/contents/stand#9"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(9)}
         />
         <area
           shape="rect"
           coords="177,589,212,613"
-          href="/contents/stand#10"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(10)}
         />
         <area
           shape="rect"
           coords="327,628,365,653"
-          href="/contents/stand#11"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(11)}
         />
         <area
           shape="rect"
           coords="368,628,405,652"
-          href="/contents/stand#12"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(12)}
         />
         <area
           shape="rect"
           coords="92,961,126,995"
-          href="/contents/stand#13"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(13)}
         />
         <area
           shape="rect"
           coords="484,629,520,653"
-          href="/contents/stand#14"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(14)}
         />
         <area
           shape="rect"
           coords="523,629,560,652"
-          href="/contents/stand#15"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(15)}
         />
         <area
           shape="rect"
           coords="559,629,597,651"
-          href="/contents/stand#16"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(16)}
         />
         <area
           shape="rect"
           coords="605,603,631,629"
-          href="/contents/stand#17"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(17)}
         />
         <area
           shape="rect"
           coords="605,578,631,604"
-          href="/contents/stand#18"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(18)}
         />
         <area
           shape="rect"
           coords="604,550,630,579"
-          href="#/contents/stand#19"
+          href="#/contents/stand"
           alt=""
+          onClick={() => onAreaClick(19)}
         />
         <area
           shape="rect"
           coords="605,523,632,551"
-          href="/contents/stand#20"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(20)}
         />
         <area
           shape="rect"
           coords="605,495,632,524"
-          href="/contents/stand#21"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(21)}
         />
         <area
           shape="rect"
           coords="605,470,630,496"
-          href="/contents/stand#22"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(22)}
         />
         <area
           shape="rect"
           coords="605,443,631,472"
-          href="/contents/stand#23"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(23)}
         />
         <area
           shape="rect"
           coords="605,416,631,444"
-          href="/contents/stand#24"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(24)}
         />
         <area
           shape="rect"
           coords="605,388,631,417"
-          href="/contents/stand#25"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(25)}
         />
         <area
           shape="rect"
           coords="605,361,630,389"
-          href="/contents/stand#26"
           alt=""
+          style={{ cursor: "pointer" }}
+          onClick={() => onAreaClick(26)}
         />
       </map>
     </>
