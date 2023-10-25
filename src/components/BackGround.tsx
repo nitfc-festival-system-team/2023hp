bf64d77dc1f4869d932bfac42ea900a83c551027
nari88118 <109565861+nari88118@users.noreply.github.com>
2023-10-26 00:56:35 +0900

    花火のサイズ縮小

--- src/components/BackGround.tsx
+++ src/components/BackGround.tsx
@@ -2,20 +2,20 @@ import React from "react";
 export const BackGroundImg = () => {
   return (
     <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
+    style={{
+      display: "flex",
+      flexDirection: "column",
+    }}
     >
       <img
         src="/image/fireworks/firework.png"
         alt="firework"
         style={{
-          maxHeight: "25vh", // 高さ
+          maxHeight: "15vh", // 高さ
           width: "auto", // 幅
           position: "absolute", // 位置の指定
           left: "53vw", // 左方向への移動
-          top: "55vh", // 上方向への移動
+          top: "40vh", // 上方向への移動
           zIndex: -2,
         }}
       />
@@ -23,7 +23,7 @@ export const BackGroundImg = () => {
         src="/image/fireworks/firework.png"
         alt="firework"
         style={{
-          maxHeight: "40vh", // 高さ
+          maxHeight: "21vh", // 高さ
           width: "auto", // 幅
           position: "absolute", // 位置の指定
           left: "55vw", // 左方向への移動
