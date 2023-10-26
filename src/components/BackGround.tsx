10c1bb13d962fa1b5303dba1c79ee8e1a009a66d
s55530sh <67565832+s55530sh@users.noreply.github.com>
2023-10-27 00:45:37 +0900

    Update BackGround.tsx

--- src/components/BackGround.tsx
+++ src/components/BackGround.tsx
@@ -5,30 +5,38 @@ export const BackGroundImg = () => {
       style={{
         display: "flex",
         flexDirection: "column",
+        position: "absolute", // 位置の指定
+        width: "100vw",
+        height: "61vw",
+        maxHeight: "45vh",
+        top: "20vh",
+        zIndex: -2,
       }}
     >
       <img
         src="/image/fireworks/firework.png"
         alt="firework"
         style={{
-          maxHeight: "15vh", // 高さ
-          width: "auto", // 幅
+          maxHeight: "20vh", // 高さ
+          height: "25vw",
+          maxWidth: "20vh", // 高さ
+          width: "25vw", // 幅
           position: "absolute", // 位置の指定
-          left: "53vw", // 左方向への移動
-          top: "40vh", // 上方向への移動
-          zIndex: -2,
+          left: "50vw", // 左方向への移動
+          bottom: "0%", // 下から何％か
         }}
       />
       <img
         src="/image/fireworks/firework.png"
         alt="firework"
         style={{
-          maxHeight: "21vh", // 高さ
-          width: "auto", // 幅
+          maxHeight: "30vh", // 高さ
+          height: "40vw",
+          maxWidth: "30vh",
+          width: "40vw", // 幅
           position: "absolute", // 位置の指定
           left: "55vw", // 左方向への移動
-          top: "20vh", // 上方向への移動
-          zIndex: -2,
+          top: "0%", // divの上はじ
         }}
       />
     </div>
