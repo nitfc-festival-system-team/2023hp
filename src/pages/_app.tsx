56bf792d550e06bd01de80b3f6cd917150f19db3
s55531sf <free34game@icloud.com>
2023-10-27 00:29:01 +0900

    npm run buildとかのコマンド全部うったよ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,16 +31,32 @@ function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=yes"
-        ></meta>
-
-<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-<link rel="manifest" href="/favicons/site.webmanifest" />
-<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-<meta name="msapplication-TileColor" content="#da532c" />
-<meta name="theme-color" content="#ffffff"></meta>
-
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <AnimatePresence mode="wait">
         <motion.div
