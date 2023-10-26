7d374246de28a02d06936ab63c8765b3c0ff9d33
s55531sf <free34game@icloud.com>
2023-10-26 23:24:22 +0900

    アイコンとタイトル変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,11 +27,20 @@ function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
   return (
     <>
       <Head>
-        <title>nitfc2023fes</title>
+        <title>福井高専高専祭2023</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=yes"
         ></meta>
+
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+<link rel="manifest" href="/favicons/site.webmanifest" />
+<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+<meta name="msapplication-TileColor" content="#da532c" />
+<meta name="theme-color" content="#ffffff"></meta>
+
       </Head>
       <AnimatePresence mode="wait">
         <motion.div
