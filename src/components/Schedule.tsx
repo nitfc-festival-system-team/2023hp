4c72c0a23c6411b30220374b84412aa391288216
void2610 <izumisan2610@gmail.com>
2023-10-26 12:04:15 +0900

    style: ボタンのスタイル変更

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -214,21 +214,31 @@ export const Schedule = () => {
     <div>
       <div style={{ height: "5vh" }}></div>
       {timeline} {/* Timelineを描画 */}
-      <ScheduleMoveButton
-        setState={setEventMove}
-        state={0}
-        buttonText={"1日目"}
-      />
-      <ScheduleMoveButton
-        setState={setEventMove}
-        state={1}
-        buttonText={"2日目"}
-      />
-      <ScheduleMoveButton
-        setState={setEventMove}
-        state={2}
-        buttonText={"3日目"}
-      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          paddingBottom: "10px",
+          paddingTop: "10px",
+          gap: isMobile ? "3vw" : "0.7vw",
+        }}
+      >
+        <ScheduleMoveButton
+          setState={setEventMove}
+          state={0}
+          buttonText={"1日目"}
+        />
+        <ScheduleMoveButton
+          setState={setEventMove}
+          state={1}
+          buttonText={"2日目"}
+        />
+        <ScheduleMoveButton
+          setState={setEventMove}
+          state={2}
+          buttonText={"3日目"}
+        />
+      </div>
       {/* 選択されたアイテムの情報を表示 */}
       {selectedItemInfo ? (
         // selectedItemInfoが真の場合の表示内容
@@ -251,7 +261,11 @@ export const Schedule = () => {
       ) : (
         // selectedItemInfoが偽の場合の表示内容
         <div style={{ marginLeft: "1rem" }}>
-          <h1>イベント名をタップで詳細表示</h1>
+          <h1>
+            イベント名をタップで
+            <br />
+            詳細表示
+          </h1>
         </div>
       )}
     </div>
